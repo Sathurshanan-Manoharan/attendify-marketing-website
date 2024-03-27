f23853694a794b1f38c439a98f03e3a618753eaf
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2024-03-27 23:43:18 +0530

    Updated page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import "./index.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
     </>
   );
