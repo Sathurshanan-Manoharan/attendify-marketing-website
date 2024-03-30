1e2e0d75b85703f7c29fb3e0ade254b7030429cf
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2024-03-30 23:24:33 +0530

    Modified the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
+import LogoCarousel from "./components/LogoCarousel";
 
 function App() {
   return (
     <>
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <h1 className="text-3xl font-bold underline">Hello attendify!</h1>
+      <button className="btn w-64 rounded-full">Button</button>
+      <LogoCarousel />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BsList, BsX } from "react-icons/bs";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,86 +9,100 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-800 shadow-lg">
+    <nav className="bg-white shadow-lg border-b-2 border-gray-300 fixed w-full z-10 mb-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <a href="#" className="text-white text-3xl font-bold">
+            <a href="#" className="text-primary text-3xl font-bold">
               Attendify
             </a>
           </div>
           <div className="hidden sm:flex sm:items-center sm:space-x-4">
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="relative text-primary px-3 py-2 rounded-full text-sm font-medium transition duration-500"
             >
               Home
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="relative text-primary px-3 py-2 rounded-full text-sm font-medium transition duration-500"
             >
               About
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="relative text-primary px-3 py-2 rounded-full text-sm font-medium transition duration-500"
             >
               Services
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="relative text-primary px-3 py-2 rounded-full text-sm font-medium transition duration-500"
             >
               Contact
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
+            </a>
+            <a
+              href="#"
+              className="relative text-primary hover:text-white hover:bg-primary inset-0 border border-primary px-3 py-1 rounded-full text-base font-medium transition duration-500 overflow-hidden"
+            >
+              GET IN TOUCH
+              <span className="absolute inset-0 border border-primary rounded-full px-3 py-2 text-base font-medium opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
           </div>
           <div className="flex items-center sm:hidden">
             <button
-              className="text-gray-300 hover:text-white focus:outline-none"
+              className="text-primary focus:outline-none"
               onClick={toggleMenu}
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
+              {isOpen ? <BsX /> : <BsList />}
             </button>
           </div>
         </div>
       </div>
 
+      {/* Responsive dropdown menu */}
       {isOpen && (
-        <div className="sm:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1">
+        <div className="sm:hidden flex flex-col items-center">
+          <div className="py-2 space-y-1">
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-primary text-center relative px-3 py-2 rounded-md text-base font-medium transition duration-500 block"
             >
               Home
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-primary text-center relative px-3 py-2 rounded-md text-base font-medium transition duration-500 block"
             >
               About
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-primary text-center relative px-3 py-2 rounded-md text-base font-medium transition duration-500 block"
             >
               Services
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-primary text-center relative px-3 py-2 rounded-md text-base font-medium transition duration-500 block"
             >
               Contact
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
+            </a>
+            <a
+              href="#"
+              className="text-primary w-64 text-center rounded-full px-3 py-2 text-base font-medium transition duration-500 relative hover:bg-primary hover:text-white block"
+            >
+              Get in touch
+              <span className="absolute inset-0 border border-primary rounded-full opacity-0 hover:opacity-100 transition duration-500"></span>
             </a>
           </div>
         </div>
