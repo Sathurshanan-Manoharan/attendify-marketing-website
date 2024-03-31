e8366d0330c6cfc7bca6785dd769c9394e109f9d
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2024-03-31 23:02:57 +0530

    Modified carousel design

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -1,59 +1,38 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import logo from "../assets/AttendifyBlueText.svg";
 
-const LogoCarousel = () => {
-  const containerRef = useRef(null);
+function LogoCarousel() {
+  const logosRef = useRef(null);
 
   useEffect(() => {
-    const animate = () => {
-      const container = containerRef.current;
-      const children = container.children;
-      let currentOffset = 0;
-
-      const moveText = () => {
-        currentOffset -= 1; // Adjust the speed by changing this value
-        if (currentOffset <= -children[0].offsetWidth) {
-          currentOffset += children[0].offsetWidth;
-          container.appendChild(children[0]); // Move the first child to the end
-        }
-        container.style.transform = `translateX(${currentOffset}px)`;
-      };
-
-      const animationId = setInterval(moveText, 30); // Adjust the speed by changing this value
-
-      // Cleanup function
-      return () => clearInterval(animationId);
-    };
-
-    animate();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    if (logosRef.current) {
+      let ul = logosRef.current;
+      ul.insertAdjacentHTML("afterend", ul.outerHTML);
+      ul.nextSibling.setAttribute("aria-hidden", "true");
+    }
   }, []);
 
   return (
-    <div className="w-full overflow-hidden carousel-container">
-      <div
-        ref={containerRef}
-        className="flex items-center"
-        style={{ whiteSpace: "nowrap" }}
+    <div
+      className="w-full inline-flex flex-nowrap overflow-hidden"
+      style={{
+        maskImage:
+          "linear-gradient(to right, transparent 0, black 128px, black calc(100% - 18px), transparent 100%)",
+      }}
+    >
+      <ul
+        ref={logosRef}
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-100 animate-infinite-scroll"
       >
-        <div className="text-slide">
-          Streamlining Attendance, Empowering Efficiency!
-        </div>
-        <div className="text-slide">
-          Streamlining Attendance, Empowering Efficiency!
-        </div>
-        <div className="text-slide">
-          Streamlining Attendance, Empowering Efficiency!
-        </div>
-        <div className="text-slide">
-          Streamlining Attendance, Empowering Efficiency!
-        </div>
-        <div className="text-slide">
-          Streamlining Attendance, Empowering Efficiency!
-        </div>
-      </div>
+        <li>
+          <img src={logo} height={20} alt="Facebook" />
+        </li>
+        <li>
+          <img src={logo} alt="Disney" />
+        </li>
+      </ul>
     </div>
   );
-};
+}
 
 export default LogoCarousel;
