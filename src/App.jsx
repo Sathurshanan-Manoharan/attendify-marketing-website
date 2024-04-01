d612bb9a4105be55a96394f5c2130c8b0f9c909a
Inoj <inojhettiarachchi@gmail.com>
2024-04-01 20:59:59 +0530

    Created timetable card

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import LogoCarousel from "./components/LogoCarousel";
 import PricingCards from "./components/PricingCards";
+import Timetable from "./components/Timetable";
+
 
 function App() {
   return (
     <>
       <Navbar />
+      
       {/* Hero Section - adib */}
       <PricingCards />
+      <Timetable/>
+     
       <LogoCarousel />
+     
+  
       {/* Footer - Binuri */}
     </>
   );
