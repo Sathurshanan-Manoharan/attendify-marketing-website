4451f7fee8a249a93856cf48820cd7fb43aa5b4b
Nethmini Apsara <nethminiapsara2022@gmail.com>
2024-04-03 11:43:35 +0530

    Created features part

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PricingCards from "./components/PricingCards";
 import Hero from "./components/Hero";
 import OurTeam from "./components/OurTeam";
 import Timetable from "./components/Timetable";
+import Features from "./components/Features";
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
       <PricingCards />
       <LogoCarousel />
       <OurTeam />
+      <Features/>
+
 
     <Timetable/>
       {/* Footer - Binuri */}
